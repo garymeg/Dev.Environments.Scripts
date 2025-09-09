f6f3ebe5ce08d78d59b69c6efd23fec8fe03bbb3
GaryMeg <github@garymeg.co.uk>
2025-09-09 07:54:08 +0100

    added "how to use to source code" Removed NES folder as it is not ready renamed Windows_Install_C64.bat to Windows_Install_Commodore.bat as it installs for C64, VIC20 and PET

--- HelloWorld/BBC_HelloWorld/HelloWorld.asm
+++ HelloWorld/BBC_HelloWorld/HelloWorld.asm
@@ -1,3 +1,21 @@
+// Hello World program for the BBC Micro
+// Assembled with Kick Assembler
+// Outputs "hello world" to the screen
+//
+// to assemble and run:
+// 1) kickass HelloWorld.asm by pressing F1 or [ctrl]+p to open command pallet
+// 2) Type "task run task" and click on "Run Task"
+// 3) select the "ICK Build & Launch BBC Emulator"
+// 4) enjoy :)
+
+// to start your own project
+// 1) copy this folder and rename it
+// 2) open the new folder in VSCode
+// 3) rename the "helloWorld.code-workspace" file to match your folder name
+// 4) rename the HelloWorld.asm file to start your own code
+// 5) Start coding :)
+
+
 .encoding "ascii" 
  
 .const OSASCI = $ffe3
--- HelloWorld/C64_HelloWorld/HelloWorld.asm
+++ HelloWorld/C64_HelloWorld/HelloWorld.asm
@@ -1,3 +1,19 @@
+// Hello World program for the C64
+// Assembled with Kick Assembler
+// Outputs "hello world" to the screen
+//
+// to assemble and run:
+// 1) kickass HelloWorld.asm by pressing F6
+// 4) enjoy :)
+
+// to start your own project
+// 1) copy this folder and rename it
+// 2) open the new folder in VSCode
+// 3) rename the "helloWorld.code-workspace" file to match your folder name
+// 4) rename the HelloWorld.asm file to start your own code
+// 5) Start coding :)
+
+
 BasicUpstart2(Start)
 .encoding "petscii_mixed"
 
